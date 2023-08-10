8dc045945a18b40ff20922a1f61fd691a1f34f2b
nofx3000 <398622407@qq.com>
2023-08-10 09:55:03 +0800

    unit 7

--- src/vocabularys/index.ts
+++ src/vocabularys/index.ts
@@ -3,10 +3,14 @@ import chapter2 from '../../src/vocabularys/2.json';
 import chapter3 from '../../src/vocabularys/3.json';
 import chapter4 from '../../src/vocabularys/4.json';
 import chapter5 from '../../src/vocabularys/5.json';
+import chapter6 from '../../src/vocabularys/6.json';
+import chapter7 from '../../src/vocabularys/7.json';
 export default {
   chapter1,
   chapter2,
   chapter3,
   chapter4,
   chapter5,
+  chapter6,
+  chapter7,
 };
